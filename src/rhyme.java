e7f395f51c413be6fb95216cf91a070942c86b89
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-10-20 01:53:27 -0700

    Added rhyme solution

--- src/rhyme.java
+++ src/rhyme.java
@@ -1,31 +1,83 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
 
 
 public class rhyme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		while (true) {
 			int n = in.nextInt();
 			if (n == 0)
 				break;
+
+            if (n == 1) {
+			    System.out.println("1 1");
+                continue;
+            }
 			
-			int answer = 1;
-			for (int i = 1; i < n; i ++) {
-				answer += factorial(i);
-			}
-			
-			System.out.println(n + " " + answer);
+            RhymeMaster rm = new RhymeMaster(n);
+			System.out.println(n + " " + rm.rhyme());
 		}
 	}
-	
-	static double factorial(int n) {
-		double res = 1;
-		for(long i = n; i > 1; i--) {
-			res = res * i;
-		}
-		return res;
-	}
+
+    public static class RhymeMaster {
+
+        int n;
+
+        public RhymeMaster(int n) {
+            this.n = n;
+        }
+
+        public int rhyme() {
+            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+            map.put(1, 1);
+            for (int i = 2; i < n; i++) {
+                map = expand_map(map);
+                print_map(map);
+            }
+            return sum(map);
+        }
+
+        public Map<Integer, Integer> expand_map(Map<Integer, Integer> map) {
+            // value, multiplicity
+            Map<Integer, Integer> new_map = new HashMap<Integer, Integer>();
+            for (Entry e : map.entrySet()) {
+                int k = (Integer) e.getKey();
+                int v = (Integer) e.getValue();
+                add_value(new_map, k+1, v);
+                add_value(new_map, k, k * v);
+            }
+            return new_map;
+        }
+
+        public static void add_value(Map<Integer, Integer> map, int key, int multiplicity) {
+            Integer m = map.get(key);
+            if (m == null) {
+                map.put(key, multiplicity);
+            } else {
+                map.put(key, m + multiplicity);
+            }
+        }
+
+        public static int sum(Map<Integer, Integer> map) {
+            int sum = 0;
+            for (Entry e : map.entrySet()) {
+                sum += (((Integer) e.getKey()) + 1) * (Integer) e.getValue();
+            }
+            return sum;
+        }
+
+        public static void print_map(Map<Integer, Integer> map) {
+            String str = "";
+            for (Entry e : map.entrySet())
+                for (int i = 0; i < ((Integer) e.getValue()); i++)
+                    str += "[1," + e.getKey() + "]";
+            System.out.println(str);
+        }
+
+    }
 
 }
