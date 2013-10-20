b335ad9cae92466b8625da3775a8bd4ac54c7af5
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-10-20 04:14:13 -0700

    Removed class from rhyme

--- src/rhyme.java
+++ src/rhyme.java
@@ -22,65 +22,46 @@ public class rhyme {
                 continue;
             }
             
-            RhymeMaster rm = new RhymeMaster(n);
-            System.out.printf(n + " %.0f%n", rm.rhyme());
+            System.out.printf(n + " %.0f%n", rhyme(n));
         }
     }
 
-    public static class RhymeMaster {
+    public static double rhyme(int n) {
+        Map<Integer, Double> map = new HashMap<Integer, Double>();
+        map.put(1, 1.0);
+        for (int i = 2; i < n; i++)
+            map = expand_map(map);
+        return sum(map);
+    }
 
-        int n;
-
-        public RhymeMaster(int n) {
-            this.n = n;
+    public static Map<Integer, Double> expand_map(Map<Integer, Double> map) {
+        // value, multiplicity
+        Map<Integer, Double> new_map = new HashMap<Integer, Double>();
+        for (Entry e : map.entrySet()) {
+            int k = (Integer) e.getKey();
+            double v = (Double) e.getValue();
+            add_value(new_map, k+1, v);
+            add_value(new_map, k, k * v);
         }
+        return new_map;
+    }
 
-        public double rhyme() {
-            Map<Integer, Double> map = new HashMap<Integer, Double>();
-            map.put(1, 1.0);
-            for (int i = 2; i < n; i++)
-                map = expand_map(map);
-            //print_map(map);
-            return sum(map);
+    public static void add_value(Map<Integer, Double> map, int key, double multiplicity) {
+        Double m = map.get(key);
+        if (m == null)
+            map.put(key, multiplicity);
+        else
+            map.put(key, m + multiplicity);
+    }
+
+    public static double sum(Map<Integer, Double> map) {
+        double sum = 0;
+        for (Entry e : map.entrySet()) {
+            int k = (Integer) e.getKey();
+            double v = (Double) e.getValue();
+            sum += (k + 1) * v;
         }
-
-        public Map<Integer, Double> expand_map(Map<Integer, Double> map) {
-            // value, multiplicity
-            Map<Integer, Double> new_map = new HashMap<Integer, Double>();
-            for (Entry e : map.entrySet()) {
-                int k = (Integer) e.getKey();
-                double v = (Double) e.getValue();
-                add_value(new_map, k+1, v);
-                add_value(new_map, k, k * v);
-            }
-            return new_map;
-        }
-
-        public static void add_value(Map<Integer, Double> map, int key, double multiplicity) {
-            Double m = map.get(key);
-            if (m == null)
-                map.put(key, multiplicity);
-            else
-                map.put(key, m + multiplicity);
-        }
-
-        public static double sum(Map<Integer, Double> map) {
-            double sum = 0;
-            for (Entry e : map.entrySet()) {
-                double k = (Integer) e.getKey();
-                double v = (Double) e.getValue();
-                sum += (k + 1) * v;
-            }
-            return sum;
-        }
-
-        public static void print_map(Map<Integer, Double> map) {
-            String str = "";
-            for (Entry e : map.entrySet())
-                str += "[" + e.getKey() + ", " + e.getValue() + "]";
-            System.out.println(str);
-        }
-
+        return sum;
     }
 
 }
