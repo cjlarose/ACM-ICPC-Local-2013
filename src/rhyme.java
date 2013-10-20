0003b02adb099773791a16dc3d1bbc0fa8b50b5d
Chris LaRose <cjlarose@iplantcollaborative.org>
2013-10-20 02:20:30 -0700

    Swapped intermediate values from int to double

--- src/rhyme.java
+++ src/rhyme.java
@@ -31,50 +31,48 @@ public class rhyme {
         }
 
         public double rhyme() {
-            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-            map.put(1, 1);
-            for (int i = 2; i < n; i++) {
+            Map<Integer, Double> map = new HashMap<Integer, Double>();
+            map.put(1, 1.0);
+            for (int i = 2; i < n; i++)
                 map = expand_map(map);
-                //print_map(map);
-            }
+            //print_map(map);
             return sum(map);
         }
 
-        public Map<Integer, Integer> expand_map(Map<Integer, Integer> map) {
+        public Map<Integer, Double> expand_map(Map<Integer, Double> map) {
             // value, multiplicity
-            Map<Integer, Integer> new_map = new HashMap<Integer, Integer>();
+            Map<Integer, Double> new_map = new HashMap<Integer, Double>();
             for (Entry e : map.entrySet()) {
                 int k = (Integer) e.getKey();
-                int v = (Integer) e.getValue();
+                double v = (Double) e.getValue();
                 add_value(new_map, k+1, v);
                 add_value(new_map, k, k * v);
             }
             return new_map;
         }
 
-        public static void add_value(Map<Integer, Integer> map, int key, int multiplicity) {
-            Integer m = map.get(key);
+        public static void add_value(Map<Integer, Double> map, int key, double multiplicity) {
+            Double m = map.get(key);
             if (m == null)
                 map.put(key, multiplicity);
             else
                 map.put(key, m + multiplicity);
         }
 
-        public static double sum(Map<Integer, Integer> map) {
+        public static double sum(Map<Integer, Double> map) {
             double sum = 0;
             for (Entry e : map.entrySet()) {
                 double k = (Integer) e.getKey();
-                double v = (Integer) e.getValue();
+                double v = (Double) e.getValue();
                 sum += (k + 1) * v;
             }
             return sum;
         }
 
-        public static void print_map(Map<Integer, Integer> map) {
+        public static void print_map(Map<Integer, Double> map) {
             String str = "";
             for (Entry e : map.entrySet())
-                for (int i = 0; i < ((Integer) e.getValue()); i++)
-                    str += "[1," + e.getKey() + "]";
+                str += "[" + e.getKey() + ", " + e.getValue() + "]";
             System.out.println(str);
         }
 
